ccfd715bce750824619022c01e01f51782dff76a
Aday <Aday@DESKTOP-V6FCD9U>
2021-03-03 16:47:27 +0000

    paso 3 y 4

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -10,6 +10,22 @@ package calculadora;
  * @author Aday
  */
 public class Calculadora {
+
+    public int getOperador1() {
+        return operador1;
+    }
+
+    public void setOperador1(int operador1) {
+        this.operador1 = operador1;
+    }
+
+    public int getOperador2() {
+        return operador2;
+    }
+
+    public void setOperador2(int operador2) {
+        this.operador2 = operador2;
+    }
     private int operador1;
     private int operador2;
     private int operacion(int a, int b){
